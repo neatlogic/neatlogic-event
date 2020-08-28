d9604204c0030713c8df83cdf91ea988923376f7
Aienao <laiwt@techsure.com.cn>
2020-08-28 17:04:59 +0800

    --story=1003221 --user=赖文韬 事件类型、解决方案管... https://www.tapd.cn/54247054/s/1085071

--- src/main/java/codedriver/module/event/api/type/EventTypeChildrenGetApi.java
+++ src/main/java/codedriver/module/event/api/type/EventTypeChildrenGetApi.java
@@ -8,10 +8,14 @@ import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 import codedriver.module.event.dao.mapper.EventTypeMapper;
 import codedriver.module.event.dto.EventTypeVo;
 import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @OperationType(type = OperationTypeEnum.SEARCH)
@@ -46,6 +50,8 @@ public class EventTypeChildrenGetApi extends PrivateApiComponentBase {
     public Object myDoService(JSONObject jsonObj) throws Exception {
     	JSONObject resultObj = new JSONObject();
 		List<EventTypeVo> eventTypeList = null;
+        Map<Long, EventTypeVo> eventTypeMap = new HashMap<>();
+        List<Long> eventTypeIdList = new ArrayList<>();
 		Long id = jsonObj.getLong("id");
 		EventTypeVo eventTypeVo = eventTypeMapper.getEventTypeById(id);
 		if(eventTypeVo == null) {
@@ -53,7 +59,30 @@ public class EventTypeChildrenGetApi extends PrivateApiComponentBase {
 		}
 
 		eventTypeList = eventTypeMapper.getChildrenByLeftRightCode(eventTypeVo.getLft(), eventTypeVo.getRht());
-		resultObj.put("children",eventTypeList);
+		if(CollectionUtils.isNotEmpty(eventTypeList)){
+            for(EventTypeVo vo : eventTypeList){
+                eventTypeMap.put(vo.getId(), vo);
+                eventTypeIdList.add(vo.getId());
+            }
+            eventTypeMap.put(eventTypeVo.getId(), eventTypeVo);
+            List<EventTypeVo> childCountList = eventTypeMapper.getEventTypeSolutionCountAndChildCountListByIdList(eventTypeIdList);
+            Map<Long, EventTypeVo> childCountMap = new HashMap<>();
+            for(EventTypeVo eventType : childCountList) {
+                childCountMap.put(eventType.getId(), eventType);
+            }
+            for(EventTypeVo eventType : eventTypeList) {
+                EventTypeVo parentEventType = eventTypeMap.get(eventType.getParentId());
+                if(parentEventType != null) {
+                    eventType.setParent(parentEventType);
+                }
+                EventTypeVo childCount = childCountMap.get(eventType.getId());
+                if(childCount != null) {
+                    eventType.setChildCount(childCount.getChildCount());
+                }
+            }
+        }
+
+		resultObj.put("children",eventTypeVo.getChildren());
     	return resultObj;
     }
 }
