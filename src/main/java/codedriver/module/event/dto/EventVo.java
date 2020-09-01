e65ba3d0be2bb6f3ecda6d1351a4b153c15eb89d
林邦泉 <linbq@techsure.com.cn>
2020-09-01 17:16:44 +0800

    --story=1003267 --user=林邦泉 事件处理节点、事件处理页面等逻辑 https://www.tapd.cn/54247054/s/1085448

--- src/main/java/codedriver/module/event/dto/EventVo.java
+++ src/main/java/codedriver/module/event/dto/EventVo.java
@@ -1,5 +1,39 @@
 package codedriver.module.event.dto;
 
+import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.restful.annotation.EntityField;
+
 public class EventVo {
 
+    @EntityField(name = "事件id", type = ApiParamType.LONG)
+    private Long id;
+    @EntityField(name = "事件归档类型id", type = ApiParamType.LONG)
+    private Long eventTypeId;
+    @EntityField(name = "事件解决方案id", type = ApiParamType.LONG)
+    private Long eventSolutionId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getEventTypeId() {
+        return eventTypeId;
+    }
+
+    public void setEventTypeId(Long eventTypeId) {
+        this.eventTypeId = eventTypeId;
+    }
+
+    public Long getEventSolutionId() {
+        return eventSolutionId;
+    }
+
+    public void setEventSolutionId(Long eventSolutionId) {
+        this.eventSolutionId = eventSolutionId;
+    }
+    
 }
