eed6bd3b82acb2a7c92c20a0e26f662668bb42a1
林邦泉 <linbq@techsure.com.cn>
2020-12-11 16:41:44 +0800

    --story=1003778 --user=林邦泉 工单处理鉴权优化 https://www.tapd.cn/54247054/s/1115080

--- src/main/java/codedriver/module/event/operationauth/handler/EventOperationAuthHandler.java
+++ src/main/java/codedriver/module/event/operationauth/handler/EventOperationAuthHandler.java
@@ -8,35 +8,25 @@ import javax.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 
 import codedriver.framework.process.constvalue.ProcessTaskOperationType;
-import codedriver.framework.process.constvalue.ProcessTaskStatus;
-import codedriver.framework.process.constvalue.ProcessUserType;
 import codedriver.framework.process.dto.ProcessTaskStepVo;
 import codedriver.framework.process.dto.ProcessTaskVo;
 import codedriver.framework.process.operationauth.core.OperationAuthHandlerBase;
 import codedriver.framework.process.operationauth.core.TernaryPredicate;
+
 @Service
 public class EventOperationAuthHandler extends OperationAuthHandlerBase {
 
-    private final Map<ProcessTaskOperationType, TernaryPredicate<ProcessTaskVo, ProcessTaskStepVo, String>> operationBiPredicateMap = new HashMap<>();
-    
+    private final Map<ProcessTaskOperationType,
+        TernaryPredicate<ProcessTaskVo, ProcessTaskStepVo, String>> operationBiPredicateMap = new HashMap<>();
+
     @PostConstruct
     public void init() {
-        
-        operationBiPredicateMap.put(ProcessTaskOperationType.CREATESUBTASK, (processTaskVo, processTaskStepVo, userUuid) -> {
-            if (processTaskStepVo.getIsActive() == 1) {
-                if (ProcessTaskStatus.RUNNING.getValue().equals(processTaskStepVo.getStatus()) || ProcessTaskStatus.DRAFT.getValue().equals(processTaskStepVo.getStatus())) {
-                    if(checkIsProcessTaskStepUser(processTaskStepVo, ProcessUserType.MAJOR.getValue(), userUuid)) {
-                        return true;
-                    }
-                }
-            }
-            return false;
-        });
-        
+
     }
-    
+
     @Override
-    public Map<ProcessTaskOperationType, TernaryPredicate<ProcessTaskVo, ProcessTaskStepVo, String>> getOperationBiPredicateMap() {
+    public Map<ProcessTaskOperationType, TernaryPredicate<ProcessTaskVo, ProcessTaskStepVo, String>>
+        getOperationBiPredicateMap() {
         return operationBiPredicateMap;
     }
 
