6d8e506869b8718a0f57f88c18b298b79d10350a
林邦泉 <linbq@techsure.com.cn>
2021-07-16 18:09:19 +0800

    --story=1005028 --user=林邦泉 工单步骤关键文案替换 https://www.tapd.cn/54247054/s/1164161

--- src/main/java/codedriver/module/event/stephandler/utilhandler/EventProcessUtilHandler.java
+++ src/main/java/codedriver/module/event/stephandler/utilhandler/EventProcessUtilHandler.java
@@ -269,9 +269,7 @@ public class EventProcessUtilHandler extends ProcessStepInternalHandlerBase {
         resultObj.put("customButtonList", customButtonArray);
 
         /** 可替换文本列表 **/
-        JSONArray replaceableTextList = configObj.getJSONArray("replaceableTextList");
-        JSONArray replaceableTextArray = ProcessConfigUtil.regulateReplaceableTextList(replaceableTextList);
-        resultObj.put("replaceableTextList", replaceableTextArray);
+        resultObj.put("replaceableTextList", ProcessConfigUtil.regulateReplaceableTextList(configObj.getJSONArray("replaceableTextList")));
 
         /** 通知 **/
         NotifyPolicyConfigVo notifyPolicyConfigVo = null;
@@ -368,9 +366,7 @@ public class EventProcessUtilHandler extends ProcessStepInternalHandlerBase {
         }
 
         /** 可替换文本列表 **/
-        JSONArray replaceableTextList = configObj.getJSONArray("replaceableTextList");
-        JSONArray replaceableTextArray = ProcessConfigUtil.regulateReplaceableTextList(replaceableTextList);
-        resultObj.put("replaceableTextList", replaceableTextArray);
+        resultObj.put("replaceableTextList", ProcessConfigUtil.regulateReplaceableTextList(configObj.getJSONArray("replaceableTextList")));
 
         /** 分配处理人 **/
         JSONObject workerPolicyConfig = configObj.getJSONObject("workerPolicyConfig");
