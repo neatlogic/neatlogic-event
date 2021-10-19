c3cb6263119b3d68fccfb72f496cf6278962a0dc
林邦泉 <linbq@techsure.com.cn>
2021-10-19 12:25:31 +0800

    --task=1005396 --user=林邦泉 后端-通知策略模板参数代码优化 https://www.tapd.cn/54247054/s/1183671

--- src/main/java/codedriver/module/event/notify/handler/EventNotifyPolicyHandler.java
+++ src/main/java/codedriver/module/event/notify/handler/EventNotifyPolicyHandler.java
@@ -162,21 +162,6 @@ public class EventNotifyPolicyHandler extends NotifyPolicyHandlerBase {
     }
 
     @Override
-    protected List<String> getMyParamNameList(){
-        List<String> resultList = new ArrayList<>();
-        for (ProcessTaskNotifyParam param : ProcessTaskNotifyParam.values()) {
-            resultList.add(param.getValue());
-        }
-        for (ProcessTaskStepNotifyParam param : ProcessTaskStepNotifyParam.values()) {
-            resultList.add(param.getValue());
-        }
-        for (TaskNotifyParam param : TaskNotifyParam.values()) {
-            resultList.add(param.getValue());
-        }
-        return resultList;
-    }
-
-    @Override
     protected List<ConditionParamVo> mySystemConditionOptionList() {
         List<ConditionParamVo> notifyPolicyParamList = new ArrayList<>();
         for (ConditionProcessTaskOptions option : ConditionProcessTaskOptions.values()) {
